c653a42d76c86f9feb22e817ad08400ccbffeb8a
tecnologosistemas <tecnologosistemas@laconcepcion.org>
2025-08-05 08:47:52 -0500

    Cambie de npm a pnpm y reslvi un error en el modulo de auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,6 @@ import { AuthController } from './auth.controller';
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
-  imports:[AuthService],
+  imports: [],
 })
 export class AuthModule {}
